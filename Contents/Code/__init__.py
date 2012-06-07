28a569774b3192514e0d8d1085011dad8a888c21
russkylapot <mrzhenya@users.noreply.github.com>
2012-06-07 08:58:47 +0000

    Added GPL v2 license to KinoPoiskRu.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+
+# Russian metadata plugin for Plex, which uses http://www.kinopoisk.ru/ to get the tag data.
+# Плагин для обновления информации о фильмах использующий КиноПоиск (http://www.kinopoisk.ru/).
+# Copyright (C) 2012  Zhenya Nyden
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import datetime, string, re, time, math, operator, unicodedata, hashlib, urlparse, types, sys
 
 ENCODING_KINOPOISK_PAGE = 'cp1251'
